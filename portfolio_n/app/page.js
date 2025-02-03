e955bca870d9fa1fd06961f4c011b0d297882fc3
hans kang <hans.kkang@gmail.com>
2025-02-03 14:41:54 -0500

    About me completed and added to the page

--- portfolio_n/app/page.js
+++ portfolio_n/app/page.js
@@ -1,14 +1,14 @@
 'use client'
 import Navbar from "../components/NavBar";
 import Header from "../components/Header";
-
+import About from "@/components/About";
 
 export default function Home() {
-
   return (
     <>
-    <Navbar/>
-    <Header/>
+      <Navbar />
+      <Header />
+      <About />
     </>
   );
 }
