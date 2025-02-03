0e7eb2289c64ce9806f587aa0594ecbebf2fbfae
hans kang <hans.kkang@gmail.com>
2025-02-03 14:23:20 -0500

    added font

--- portfolio_n/app/layout.js
+++ portfolio_n/app/layout.js
@@ -1,15 +1,17 @@
-import { Geist, Geist_Mono } from "next/font/google";
+import { Outfit, Ovo} from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
+
+const outfit = Outfit({
+  subsets: ["latin"], weight: ["400", "500", "600", "700"]
 });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+const ovo = Ovo({
+  subsets: ["latin"], weight: ["400"]
 });
+
+
+
 
 export const metadata = {
   title: "Create Next App",
