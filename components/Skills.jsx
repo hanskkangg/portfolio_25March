a2dead040d13840684fc7212253b5515987ceb30
hans kang <hans.kkang@gmail.com>
2025-03-10 23:04:50 -0400

    working on gaps and pt

--- components/Skills.jsx
+++ components/Skills.jsx
@@ -119,7 +119,7 @@ const Category = ({ number, title, children }) => (
   whileInView={{ opacity: 1 }}
   transition={{ duration: 0.6, delay: 0.1 }}
   className="flex flex-col border-b border-black dark:border-gray-600 pb-5
-  xl:flex-row xl:items-center xl:justify-start xl:gap-4 xl:w-[100%] xl:pb-8 xl:pt-8 xl:mb-0 xl:mt-1" 
+  xl:flex-row xl:items-center xl:justify-start xl:gap-2 xl:w-[100%] xl:pb-8 xl:pt-2 xl:mb-0 xl:mt-1" 
 >
     {/* Number with stroke effect */}
     <div className="flex items-center xl:flex-row xl:items-center xl:w-full">
