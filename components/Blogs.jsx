2b812a5cf9b7c9369f9870984fd61b8d54cecb63
hans kang <hans.kkang@gmail.com>
2025-04-01 16:22:39 -0400

    border appears only for dark mode

--- components/Blogs.jsx
+++ components/Blogs.jsx
@@ -53,7 +53,7 @@ BLOGS
             href={blog.blogUrl}
             target="_blank"
             rel="noopener noreferrer"
-            className="border  border-gray-500 shadow-2xl rounded-lg overflow-hidden  hover:shadow-xl transition-transform transform hover:-translate-y-2"
+            className="border dark:border-gray-500 shadow-2xl rounded-lg overflow-hidden  hover:shadow-xl transition-transform transform hover:-translate-y-2"
           >
             <img
               src={blog.imageUrl}
